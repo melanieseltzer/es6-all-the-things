1139bf425e2a74f84bc450e8d9ee046804d9e1d1
Melanie Seltzer <melleh11@gmail.com>
2018-05-25 12:02:23 -0700

    Updates

--- path----665809ea1f53a04bd372.js
+++ path----665809ea1f53a04bd372.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"ES6 All The Things"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{79:function(t,e){t.exports={data:{site:{siteMetadata:{title:"ES6 All The Things"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----665809ea1f53a04bd372.js.map
